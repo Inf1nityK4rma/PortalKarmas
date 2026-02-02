ccf32b43e3ad2bd6d2e2d4e730e9ed321b12ac8c
Soma <diegogiyuu097@gmail.com>
2026-02-02 01:56:32 -0400

    ahora podrá verse sin el botón

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,24 @@ function App() {
       <button>
   <a href="/tutorialEPICO/Setso.tsx"></a>
 </button>
+
+<div style={{
+background: 'blue',
+padding:'20px',
+textAlign:'center',
+animation:'blink 1s infinite',
+fontFamily:'Arial, sans-serif',
+color:'white'
+}}>
+<style>
+        {`@keyframes blink { 50% { opacity: 0; } }`}
+      </style>
+      <h1 style={{ fontStyle: 'italic' }}>
+        FSDFISDFJSAI
+        </h1> 
+        <img src="/xd.png" alt="Puto" width={200} />
+</div>
+
     </>
   )
 }
